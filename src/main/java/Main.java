51a4201f4ae4d0064662e74aa3073a9f6b86fa88
php <o-knyazev@mail.ru>
2015-07-18 15:40:18 +0300

    lecture 10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,4 +6,26 @@
  * @since 15.07.2015
  */
 public class Main {
+   private static int count;
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Main main2=new Main();
+
+        System.out.println("Main created " + count + " again" );
+    }
+
+    public Main() {
+        count++;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Main.count = count;
+    }
+
+
 }
