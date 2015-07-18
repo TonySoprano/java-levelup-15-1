feb2358c8e088f44e68f340b7d96376a39aa4e26
Макуцкий Артем <makutskiy23@gmail.com>
2015-07-19 00:14:45 +0300

    Lecture10 modify makutskiy_artem

---
+++ src/main/java/lecture10/Counter.java
@@ -0,0 +1,24 @@
+package lecture10;
+
+/**
+ * @version 1.0
+ * @auhtor Artem Makutskiy
+ * @since 19.07.2015.
+ */
+public class Counter {
+
+    private static int count;
+
+    public Counter () {
+        counterAdd();
+    }
+
+    public void counterAdd () {
+        count++;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+}
--- src/main/java/lecture10/Main.java
+++ src/main/java/lecture10/Main.java
@@ -6,20 +6,11 @@ package lecture10;
  * @since 18.07.2015.
  */
 public class Main {
-    private static int count;
-
-    public Main () {
-        count++;
-    }
-
-    public static int getCount() {
-        return count;
-    }
 
     public static void main(String[] args) {
         for (int i =0; i < 5; i++) {
-            new Main();
+            new Counter();
         }
-        System.out.println(Main.getCount());
+        System.out.println(Counter.getCount());
     }
 }
