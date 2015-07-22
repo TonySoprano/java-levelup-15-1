7c30a31b16a535ecb3ea9be7d6b8f4db21ef27bb
php <o-knyazev@mail.ru>
2015-07-22 20:50:59 +0300

    class work

---
+++ src/main/java/lecture11/Container.java
@@ -0,0 +1,20 @@
+package lecture11;
+
+/**
+ * Created by Oleg Knyazev
+ * on 22.07.2015.
+ */
+public class Container {
+    private Object[] container = new Object[10];
+    private int i = 0;
+
+    public int add(Object object) {
+
+
+    }
+
+    public Object get(int index) {
+        return container[index];
+    }
+
+}
