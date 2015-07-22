b1238c61d2c0aae862b7bb20c5a05a0ba7f22925
Timale02 <Timoshenko.alex@yahoo.com>
2015-07-22 21:05:50 +0300

    lacture11.1

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Class {@link Main}
+ *
+ * @author Oleg Kalinichenko
+ * @version 1.0
+ * @since 15.07.2015
+ */
+public class Main {
+
+}
---
+++ src/main/java/timoshrnko_alexey/lacture11/GoodContainer.java
@@ -0,0 +1,15 @@
+package timoshrnko_alexey.lacture11;
+
+/**
+ * Created by php on 22.07.2015.
+ */
+public class GoodContainer<t> {
+private t[] container;
+
+public  void addIndex(t args){
+    container = (t[]) new Object[1];
+}
+    public void getIndex
+
+
+}
