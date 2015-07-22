2605d8d1543e66ff42334780bbf020f9d9d916aa
php <skityashin@gmail.com>
2015-07-22 21:01:32 +0300

    lecture11

---
+++ src/main/java/lecture11/Container.java
@@ -0,0 +1,34 @@
+package main.java.lecture11;
+
+public class Container {
+    private Object[] mass = new Object[10];
+    private int i = 0;
+
+    public int add(Object object) {
+        mass[i] = object;
+//        if (i == 11) {
+//            Object[] temp = mass;
+//            mass = new Object[2];
+//            mass[i] = object;
+//        }
+        return i++;
+    }
+
+    //    public void setMass(Object[] mass) {
+//        this.mass = mass;
+//    }
+//    public Object[] getMass() {
+//        return mass;
+//    }
+    public Object get(int index) {
+        return mass[index];
+    }
+
+    public void del() {
+        for (int t = 0; t < mass.length; t++) {
+            mass[t] = null;
+        }
+        i=0;
+    }
+
+}
