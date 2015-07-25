c1e53a11a74897a6bc8d5e69f58ccc2a9a279ba7
java <romiro0@gmail.com>
2015-07-25 15:59:43 +0300

    lecture 11

--- src/main/java/lecture11/Part2/Conteiner.java
+++ src/main/java/lecture11/Part2/Conteiner.java
@@ -1,17 +1,31 @@
 package Lecture11.Part2;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Created by java on 22.07.2015.
  */
 public class Conteiner <T> {
-    private T[] conteiner = new T[10];
+    private T[] conteiner = (T[]) new Object[3] ;
     private int i =0;
 
-    public T[] getConteiner(int index) {
-        return (T[]) conteiner[index];
+    public T getConteiner(int index) {
+
+        return  conteiner[index];
+
     }
 
     public void setConteiner(T conteiner) {
-        this.conteiner [i++] = conteiner;
+        if(i>=this.conteiner.length){
+            T[] temp = this.conteiner;
+            this.conteiner = (T[]) new Objects[this.conteiner.length*2];
+            this.conteiner = Arrays.copyOf(temp,temp.length);
+            System.out.println(this.conteiner);
+            this.conteiner [i++] = conteiner;
+        }else{
+            this.conteiner [i++] = conteiner;
+        }
     }
 }
--- src/main/java/lecture11/Part2/Main.java
+++ src/main/java/lecture11/Part2/Main.java
@@ -5,6 +5,18 @@ package Lecture11.Part2;
  */
 public class Main {
     public static void main(String[] args) {
+    Conteiner <Integer> ob = new Conteiner<Integer>();
+
+        ob.setConteiner(1);
+        ob.setConteiner(2);
+        ob.setConteiner(3);
+        ob.setConteiner(4);
+        ob.setConteiner(5);
+        ob.setConteiner(6);
+
+
+        System.out.println(ob.getConteiner(5));
+//        System.out.println(ob.getConteiner(1));
 
     }
 }
