20844b0911012439e9d3b97e4ad382d49110140f
php <o-knyazev@mail.ru>
2015-08-15 15:49:38 +0300

    class work

---
+++ src/main/java/lecture13/Main.java
@@ -0,0 +1,22 @@
+package lecture13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+/**
+ * Created by Oleg Knyazev
+ * on 15.08.2015.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("enter command :");
+        String number = reader.readLine();
+        String[] Arr= number.split(" ");
+
+
+
+
+    }
+}
