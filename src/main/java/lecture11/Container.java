824b03d4bb5a1552a5dced0d7eb0c286edf61b80
php <o-knyazev@mail.ru>
2015-08-15 14:46:57 +0300

    class work

--- src/main/java/lecture11/Container.java
+++ src/main/java/lecture11/Container.java
@@ -8,10 +8,6 @@ public class Container {
     private Object[] container = new Object[10];
     private int i = 0;
 
-    public int add(Object object) {
-
-
-    }
 
     public Object get(int index) {
         return container[index];
