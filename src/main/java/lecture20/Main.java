932e0d33760c606228a0043e753fa034460ef097
php <skityashin@gmail.com>
2015-09-30 21:01:10 +0300

    lecture20

--- src/main/java/lecture20/Main.java
+++ src/main/java/lecture20/Main.java
@@ -6,14 +6,21 @@ public class Main {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/company", "root", "root"); //;
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/company", "root", "root");
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM employees");
+            ResultSet rs = stmt.executeQuery("SELECT employee_id, COUNT(employee_id) AS n FROM courses2staff GROUP BY employee_id HAVING n = 2;");
+            System.out.printf("%10s | ", rs.getMetaData().getColumnName(1));
+            System.out.printf("%11s | ", rs.getMetaData().getColumnName(2));
+            System.out.println();
+            System.out.println("---------------------------");
             while (rs.next()) {
-                for (int i = 1; i < 9; i++) {
-                    System.out.print(rs.getString(i) + " ");
+
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+
+                    System.out.printf("%11s | ", rs.getString(i));
                 }
                 System.out.println();
+                System.out.println("---------------------------");
             }
             rs.close();
             stmt.close();
